06a7fdba5d22e60f4c4a255ff58748c9df7d8be2
jrodt333 <jrodt333@gmail.com>
2021-01-22 20:47:45 -0800

    fixed reply.py, should now respond to certain variations of tagging the bot and saying 'Hello there!'

--- reply.py
+++ reply.py
@@ -10,19 +10,28 @@ auth = tweepy.OAuthHandler("j61yE6eUNZsVtNM2RKP3yPcr5",
 auth.set_access_token("1308162909965443073-ZIAawyZNY97l5ZxArCYaooVXCwVH7p", 
     "qKDZpiIVKZkzplxRjCOgPid6ylPOAHGkCzonQuSYeDLqH")
 api = tweepy.API(auth)
-twt = api.search(q="Hello There!")
+#twt = api.search(q="Hello There!")
+twt = api.search(q="@prequelmemebot1")
 
 #list of specific strings we want to check for in Tweets
 t = ['Hello There!',
-    'Hello There!',
+    'Hello there!',
     'Hello There!!!',
-    'Hello There!!!',
+    'Hello there!!!',
     'Hello, There!',
-    'Hello, There!']
+    'Hello, there!']
 
+#print(twt)
 for s in twt:
+    #print(s.text)
     for i in t:
-        if i == s.text:
+        #print(i)
+        #print(s.text)
+        if '@prequelmemebot1 ' + i == s.text:
             sn = s.user.screen_name
-            m = "General @%s Hahahaha... You are a bold one." % (sn)
-            s = api.update_status(m, s.id)
+            m = "General @%s. You are a bold one." % (sn)
+            print(m)
+            try:
+                s = api.update_status(m, s.id)
+            except tweepy.error.TweepError as e:
+                print(e)
